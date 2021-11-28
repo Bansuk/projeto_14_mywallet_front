126eff4084b57ecdb29e740a5f700363df683d2e
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-11-28 13:41:37 -0300

    Change component declaration of balance route

--- src/Services/api.services.js
+++ src/Services/api.services.js
@@ -1,42 +1,42 @@
-import axios from "axios";
+import axios from 'axios';
 
-const URL = "http://localhost:4000/";
+const URL = 'http://localhost:4000/';
 
 const config = token => {
-    return {
-        headers: { Authorization: `Bearer ${token}` },
-    };
+  return {
+    headers: { Authorization: `Bearer ${token}` },
+  };
 };
 
 const signInUser = user => {
-    return axios.post(`${URL}signin`, user);
+  return axios.post(`${URL}sign-in`, user);
 };
 
 const signUpUser = user => {
-    return axios.post(`${URL}signup`, user);
+  return axios.post(`${URL}sign-up`, user);
 };
 
 const signOutUser = token => {
-    return axios.delete(`${URL}signout`, config(token));
+  return axios.delete(`${URL}sign-out`, config(token));
 };
 
 const getTransactions = token => {
-    return axios.get(`${URL}transactions`, config(token));
+  return axios.get(`${URL}transactions`, config(token));
 };
 
 const getBalance = token => {
-    return axios.get(`${URL}balance`, config(token));
+  return axios.get(`${URL}balance`, config(token));
 };
 
 const postTransaction = (body, token) => {
-    return axios.post(`${URL}transaction`, body, config(token));
+  return axios.post(`${URL}transaction`, body, config(token));
 };
 
 export {
-    signInUser,
-    signUpUser,
-    signOutUser,
-    getTransactions,
-    getBalance,
-    postTransaction,
+  signInUser,
+  signUpUser,
+  signOutUser,
+  getTransactions,
+  getBalance,
+  postTransaction,
 };
